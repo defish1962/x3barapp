38cd219f030f2d47caa8557af6ca59439a794197
David Fish <david.fish@toptal.com>
2021-02-19 10:23:47 -0700

    Initial Commit

---
+++ X3Bar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  X3Bar
+//
+//  Created by David Fish on 2/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ X3Bar/X3BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  X3BarApp.swift
+//  X3Bar
+//
+//  Created by David Fish on 2/19/21.
+//
+
+import SwiftUI
+
+@main
+struct X3BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
